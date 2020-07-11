810225f622be466d974d1684a058e2cc071129d3
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-07-11 21:36:25 +0530

    specific folder upload added

--- course/admin.py
+++ course/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from course.models import Course, Assignment
+from course.models import Course, Assignment, ExamPaper
 
 admin.site.register(Course)
 admin.site.register(Assignment)
+admin.site.register(ExamPaper)
--- course/models.py
+++ course/models.py
@@ -1,15 +1,31 @@
 from django.db import models
 
-def get_upload_path(instance, filename):
-    return '{0}/{1}'.format(instance.course.course_code, filename)
+def get_upload_path_assignment(instance, filename):
+    return 'Assignment_{0}/{1}'.format(instance.course.course_code, filename)
+
+def get_upload_path_exam(instance, filename):
+    return 'Exam_{0}/{1}'.format(instance.course.course_code, filename)
 
 class Course(models.Model):
     course_code = models.CharField(max_length=10)
     course_name = models.CharField(max_length=100)
     instructor = models.CharField(max_length=25)
 
+    def __str__(self):
+        return f'{self.course_code}'
+
 class Assignment(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     heading = models.CharField(max_length=50)
-    attachment = models.FileField(upload_to=get_upload_path)
+    attachment = models.FileField(upload_to=get_upload_path_assignment)
 
+    def __str__(self):
+        return f'{self.heading}'
+
+class ExamPaper(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    heading = models.CharField(max_length=50)
+    attachment = models.FileField(upload_to=get_upload_path_exam)
+
+    def __str__(self):
+        return f'{self.heading}'
