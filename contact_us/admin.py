4cd2123ba08479919063b94c15d9da6b87beeb08
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-07-12 23:11:01 +0530

    contribute to model created

--- contact_us/admin.py
+++ contact_us/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from contact_us.models import Contact
 
-# Register your models here.
+admin.site.register(Contact)
--- contact_us/models.py
+++ contact_us/models.py
@@ -4,8 +4,9 @@ from course.models import Course
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
+    field = models.CharField(max_length=20)
     suggestion = models.CharField(max_length=200)
 
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.name} {self.field}'
