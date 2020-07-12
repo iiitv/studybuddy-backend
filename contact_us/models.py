8601fe1579794bf80acf6b12ae2937189f744bd9
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-07-12 22:48:17 +0530

    contact us model created

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from semester.models import Semester
+from course.models import Course
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    suggestion = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return f'{self.name}'
