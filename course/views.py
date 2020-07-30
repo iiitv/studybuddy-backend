b9374b2c0c0ee57e1b2a825dcb59eab02b238ae3
Shivam Balwani <48817020+balwanishivam@users.noreply.github.com>
2020-07-30 20:27:54 +0530

    Urls Authentication

--- course/views.py
+++ course/views.py
@@ -1,22 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
 from course.models import Course, Assignment, ExamPaper, VideoLecture
 from course.serializers import CourseSerializers, AssignmentSerializers, ExamPaperSerializers, VideoLectureSerializers 
-
+from rest_framework.decorators import  permission_classes
+from rest_framework.permissions import IsAuthenticated
 class CourseViewSets(ModelViewSet):
+    permission_classes =[IsAuthenticated]
     serializer_class = CourseSerializers
     queryset = Course.objects.all()
 
 
 class AssignmentViewSets(ModelViewSet):
+    permission_classes =[IsAuthenticated]
     serializer_class = AssignmentSerializers
     queryset = Assignment.objects.all()
 
 
 class ExamPaperViewSets(ModelViewSet):
+    permission_classes =[IsAuthenticated]
     serializer_class = ExamPaperSerializers
     queryset = ExamPaper.objects.all()
 
 
 class VideoLectureViewSets(ModelViewSet):
+    permission_classes =[IsAuthenticated]
     serializer_class = VideoLectureSerializers
     queryset = VideoLecture.objects.all()
--- semester/views.py
+++ semester/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from semester.models import Semester
 from semester.serializers import SemesterSerializers
-
+from rest_framework.decorators import  permission_classes
+from rest_framework.permissions import IsAuthenticated
 class SemesterViewSets(ModelViewSet):
+    permission_classes =[IsAuthenticated]
     serializer_class = SemesterSerializers
     queryset = Semester.objects.all()
