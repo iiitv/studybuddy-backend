e70e6eef4e9cd72da1b36b52b5a96e2ef12bfc1c
Krishna Gaur <gaurkrishna498@gmail.com>
2020-09-03 16:28:03 +0530

    Added dummy data for model testing and changed settings

--- api/settings.py
+++ api/settings.py
@@ -23,8 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+#DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
