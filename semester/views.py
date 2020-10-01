29151c3b866bed52ff73dc0c3b69d5392f4d6dbf
Yash Shah <201851150@iiitvadodara.ac.in>
2020-10-01 10:04:49 +0530

    added api test for semester model

--- semester/views.py
+++ semester/views.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from semester.models import Semester
 from semester.serializers import SemesterSerializers
 
+
 class SemesterViewSets(ModelViewSet):
     serializer_class = SemesterSerializers
     queryset = Semester.objects.all()
